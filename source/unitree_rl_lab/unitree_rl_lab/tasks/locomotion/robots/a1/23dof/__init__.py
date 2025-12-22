34e459ac66be7435db17ab308a41bd9ba613e575
Anton Monakhov <a.monahov@tb-drone.ru>
2025-12-22 12:04:40 +0300

    amp policy

--- source/unitree_rl_lab/unitree_rl_lab/tasks/locomotion/robots/a1/23dof/__init__.py
+++ source/unitree_rl_lab/unitree_rl_lab/tasks/locomotion/robots/a1/23dof/__init__.py
@@ -10,3 +10,14 @@ gym.register(
         "rsl_rl_cfg_entry_point": f"unitree_rl_lab.tasks.locomotion.agents.rsl_rl_ppo_cfg:BasePPORunnerCfg",
     },
 )
+
+gym.register(
+    id="Unitree-Arcus-A1-v0-AMP-Velocity",
+    entry_point="isaaclab.envs:ManagerBasedRLEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": f"{__name__}.amp_velocity_env_cfg:RobotEnvCfg",
+        "play_env_cfg_entry_point": f"{__name__}.amp_velocity_env_cfg:RobotPlayEnvCfg",
+        "rsl_rl_cfg_entry_point": f"unitree_rl_lab.tasks.locomotion.agents.rsl_rl_ppo_cfg:BasePPORunnerCfg",
+    },
+)
